6744b5ee0d786bd21a103905fcbe2cb4caceefc4
Jaime Alexander Uribe <jaimealexanderuribe@gmail.com>
2020-10-17 20:42:05 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jaime Uribe on 17/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
